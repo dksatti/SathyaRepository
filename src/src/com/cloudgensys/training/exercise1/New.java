a78795e18ec8e14fbe481d4572e7fec52847eb78
pnarendra578 <narendra.pakanati@cloudgensys.com>
2016-03-15 23:45:08 +0530

    #1 - Creation of New.java file

---
+++ src/src/com/cloudgensys/training/exercise1/New.java
@@ -0,0 +1,10 @@
+package src.com.cloudgensys.training.exercise1;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello");
+	}
+
+}
